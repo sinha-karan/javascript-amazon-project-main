9ffc689d783979ec86c2c65d10c711837ee3f25c
Karan Sinha <0308karan.sinha@gmail.com>
2025-05-17 18:20:42 +0530

    Save data and generate HTML for Delivery options

--- data/cartAmazon.js
+++ data/cartAmazon.js
@@ -4,9 +4,11 @@ if (!cart) {
   cart = [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
+    deliveryOptionId: '1'
   },{
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1
+    quantity: 1,
+    deliveryOptionId: '2'
   }];
 }
 
@@ -30,7 +32,8 @@ export function addToCart(productID){
   } else{ 
     cart.push({
       productId: productID,
-      quantity: 1
+      quantity: 1, 
+      deliveryOptionId: '2'
     });
   }
 
