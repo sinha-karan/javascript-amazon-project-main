86dfefa81de1141015c2181d847af2ac0ca72046
Karan Sinha <0308karan.sinha@gmail.com>
2025-05-24 10:48:54 +0530

    Rename tests-jasmine folder to tests

--- tests/tests-simple/moneyTest.js
+++ tests/tests-simple/moneyTest.js
@@ -1,4 +1,4 @@
-import formatCurrency from "../Scripts/utils/money.js";
+import formatCurrency from "../../Scripts/utils/money.js";
 
 if (formatCurrency(2095) === '20.95') {
   console.log('Passed');
