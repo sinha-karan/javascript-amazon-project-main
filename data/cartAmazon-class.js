e3bc582a5f028c60c62b2f6cea9201a7f7518b85
Karan Sinha <0308karan.sinha@gmail.com>
2025-05-26 23:54:28 +0530

    Make localStorageKey and loadFromStorage private

--- data/cartAmazon-class.js
+++ data/cartAmazon-class.js
@@ -1,14 +1,14 @@
 class Cart{
   cartItems;
-  localStorageKey;
+  #localStorageKey;
 
   constructor(localStorageKey){
-    this.localStorageKey = localStorageKey;
-    this.loadFromStorage();
+    this.#localStorageKey = localStorageKey;
+    this.#loadFromStorage();
   }
 
-  loadFromStorage() {
-    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+  #loadFromStorage() {
+    this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey));
 
     if (!this.cartItems) {
       this.cartItems = [{
@@ -24,7 +24,7 @@ class Cart{
   }
 
   saveToStorage() {
-    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+    localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
   }
 
   addToCart(productID){
@@ -82,5 +82,7 @@ class Cart{
 const cart = new Cart('cart-oop');
 const businessCart = new Cart('cart-business');
 
+cart.addToCart('83d4ca15-0f35-48f5-b7a3-1ea210004f2e');
+ 
 console.log(cart);
 console.log(businessCart);
