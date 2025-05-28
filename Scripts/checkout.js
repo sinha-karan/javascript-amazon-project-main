a9eda1e07bc59081fa93e4828eca81d9f1befa99
Karan Sinha <0308karan.sinha@gmail.com>
2025-05-28 12:26:12 +0530

    Converts product into classes

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import '../data/cartAmazon-class.js';
+// import '../data/cartAmazon-class.js';
 
 renderOrderSummary();
 
