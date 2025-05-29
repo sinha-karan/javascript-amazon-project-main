b96b77e5d4235b9cbff629392ab008db0bf8bae8
Karan Sinha <0308karan.sinha@gmail.com>
2025-05-30 02:52:00 +0530

    Learn Promises and convert code to promises

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -3,8 +3,49 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // import '../data/cartAmazon-class.js';
 // import '../data/backend-practice.js'
 import { loadProducts } from "../data/allProducts.js";
+import { loadCart } from "../data/cartAmazon.js";
 
-loadProducts(() => {
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve('value1');
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+]).then((values) => {
+  console.log(values)
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve();
+  });
+
+}).then(() => {
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+}).then(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+*/
+
+// Callback is used below
+
+// loadProducts(() => {
+//   loadCart(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+//   });
+// });
