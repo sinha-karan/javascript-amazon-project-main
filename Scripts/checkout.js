e9953f7ddfb2c0d078cfd03cf677dac4aa3619e7
Karan Sinha <0308karan.sinha@gmail.com>
2025-05-29 15:11:34 +0530

    Add Backend practice file

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // import '../data/cartAmazon-class.js';
+import '../data/backend-practice.js'
 
 renderOrderSummary();
 
