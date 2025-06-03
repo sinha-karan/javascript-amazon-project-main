6217334b7bcd5f3020e4aec4de71b2ade9d0a570
Karan Sinha <0308karan.sinha@gmail.com>
2025-06-03 12:16:04 +0530

    convert from promises to async await

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -5,6 +5,21 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts, loadProductsFetch } from "../data/allProducts.js";
 import { loadCart } from "../data/cartAmazon.js";
 
+async function loadPage() {
+  await loadProductsFetch();
+
+  await new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+loadPage();
+
+/*
 Promise.all([
   loadProductsFetch(),
   new Promise((resolve) => {
@@ -17,6 +32,7 @@ Promise.all([
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
 
 /*
 new Promise((resolve) => {
